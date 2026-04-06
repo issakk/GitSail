20051a1398f421998edb9bf03f535c4f681542eb
huangjianchao <huangjianchao@wisight.cn>
2026-04-06 18:52:52 +0800

    chore: upgrade to Tauri v2

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -11,6 +11,9 @@ use commands::*;
 
 fn main() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_fs::init())
         .invoke_handler(tauri::generate_handler![
             // 项目相关
             get_project_info,
